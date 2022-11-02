eb25abf6fe4a645fd2ec2ffb9b206715f08d171d
Ekaterina Greben <katenka26@gmail.com>
2022-11-02 20:02:50 +0200

    NopCommerce auto test in C# has been written

---
+++ UnitTest1.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+using NUnit.Allure.Core;
+using NUnit.Allure.Attributes;
+
+namespace nopCommerceAuto_tests;
+
+[AllureNUnit]
+[AllureSuite("LoginTest")]
+public class Tests
+{
+    
+    EdgeDriver? driver;
+    
+    [SetUp]
+    public void Setup()
+    {
+        this.driver = new EdgeDriver();
+        this.driver.Url = "http://192.168.56.101:8088/";
+    }
+
+    [Test]
+    public void Login()
+    {
+        
+        // go to login page
+        this.driver.FindElement(By.XPath("//a[@class='ico-login']")).Click();
+        Thread.Sleep(1000);
+
+        // fill form fields
+
+        // login
+        IWebElement loginField = this.driver.FindElement(By.XPath("//input[@id='Email']"));  
+        loginField.Clear();
+        loginField.SendKeys("greben@localnet.ua");
+
+        // password
+        IWebElement passwordField = this.driver.FindElement(By.XPath("//input[@id='Password']"));  
+        passwordField.Clear();
+        passwordField.SendKeys("Pa$$w0rd");
+
+
+        // login button
+        this.driver.FindElement(By.XPath("//button[normalize-space()='Log in']")).Click();
+        
+
+        IWebElement logoutLink = this.driver.FindElement(By.XPath("//a[@class='ico-logout']"));
+        System.Console.WriteLine(logoutLink.Text);
+        Assert.That(logoutLink.Text == "LOG OUT", "You are not logged!");
+        
+        Thread.Sleep(3000);
+    }
+
+    [TearDown]
+    public void endTests() {
+        this.driver.Close();
+    }
+}
+
